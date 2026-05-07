3fbd39db86622e7c4956a4297dbea2a4c6e950c8
Shivam Pandey <shivam.pandey1@daffodilsw.com>
2026-05-07 18:22:17 +0530

    -- Added Env

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+print('DB_HOST: ', bool(DB_HOST))
+print('DB_PORT: ', bool(DB_PORT))
+print('DB_NAME: ', bool(DB_NAME))
+print('DB_USER: ', bool(DB_USER))
+print('DB_PASSWORD: ', bool(DB_PASSWORD))
 
 app = FastAPI()
 

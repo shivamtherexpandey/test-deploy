2fe73272ccf279c5f457d56a7e95b5de7bfa894c
Shivam Pandey <shivam.pandey1@daffodilsw.com>
2026-05-07 18:38:56 +0530

    -- Applied Updated changes.

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-
-print('DB_HOST: ', bool(DB_HOST))
-print('DB_PORT: ', bool(DB_PORT))
-print('DB_NAME: ', bool(DB_NAME))
-print('DB_USER: ', bool(DB_USER))
-print('DB_PASSWORD: ', bool(DB_PASSWORD))
-
 app = FastAPI()
 
 @app.get("/health")
 def health():
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+    print('DB_HOST: ', bool(DB_HOST))
+    print('DB_PORT: ', bool(DB_PORT))
+    print('DB_NAME: ', bool(DB_NAME))
+    print('DB_USER: ', bool(DB_USER))
+    print('DB_PASSWORD: ', bool(DB_PASSWORD))
+
     return {"status": "ok"}
